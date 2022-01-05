36ab76452265e5e408b598e6cc4d33999c21bcd9
bhure01 <dattatribhure@gmail.com>
2022-01-05 16:24:57 +0530

    Added: Gambler Resign for the day if won or loss 50% of the stack

--- src/com/bridgelabzs/GamblingSimulator.java
+++ src/com/bridgelabzs/GamblingSimulator.java
@@ -4,6 +4,12 @@ import java.util.*;
 public class GamblingSimulator {
    public static final int PER_DAY_STACK_COST = 100;
    public static final int BET_COST = 1;
+   public static final int BET_WIN = 1;
+
+   private static int lossStack = 0;
+   private static int winStack = 0;
+
+    private static Random randStatus = new Random();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler simulator program");
@@ -16,6 +22,26 @@ public class GamblingSimulator {
             System.out.println("Gambler Wins");
         } else {
             System.out.println("Gambler Looses");
+        }
+    }
+    private static void playGame() {
+        while(true) {
+            int gameStatus = randStatus.nextInt(2) + 1;
+            if (gameStatus == BET_WIN) {
+                winStack += BET_COST;
+                System.out.println("Won the Game - WIN STACK : " + winStack);
+            } else {
+                lossStack += BET_COST;
+                System.out.println("Lost the Game - LOOSE STACK :" + lossStack);
+            }
+            if(winStack == PER_DAY_STACK_COST/ 2) {
+                System.out.println("Win Stack is 50% of per day stack cost and Gambler Resigning for day");
+                break;
+            }
+            if(lossStack == PER_DAY_STACK_COST / 2) {
+                System.out.println("Loss Stack is 50% of per day stack and Gambler is Resigning for the day");
+                break;
+            }
         }
     }
 }
